69a2181cb6e519cae5595cfb1d351b2faa08f4ab
Sota Yamashita <sota.yamashita@gmail.com>
2015-03-24 15:03:45 +0900

    Update lib/presigner.rb

--- lib/presigner.rb
+++ lib/presigner.rb
@@ -6,10 +6,18 @@ module Presigner
 
     desc "url ACCESS_KEY_ID SECRET_ACCESS_KEY DURATION BUCKET KEY", "Create presigned URLs for S3 operations"
     def url(access_key_id, secret_access_key, duration, bucket, key)
-        signer = Aws::S3::Client.new(access_key_id, secret_access_key)
-        url    = signer.presigned_url(:get_object, bucket: bucket, key: key)
+
+				s3 = Aws::S3::Client.new(
+					access_key_id: access_key_id,
+					secret_access_key: secret_access_key,
+					region: "ap-northeast-1"
+				)
+
+				signer = Aws::S3::Presigner.new(client: s3)
+        url    = signer.presigned_url(:get_object, bucket: bucket, key: key, expires_in: duration)
 
         say url, :green
+
     end
 
   end
